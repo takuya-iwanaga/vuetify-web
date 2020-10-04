b253b22ceea258c966921e486a9dc331f34f05b9
takuya-iwanaga <nishi.iwa0901@gmail.com>
2020-10-04 18:59:17 +0900

    vuetifyを用いたサンプルアプリ

---
+++ vuetify_lesson/src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <v-container>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="title grey--text text--darken-2">
+          Navigation lists
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+
+     <v-list dense nav>
+       <v-list-group v-for="fugu1 in fugus1"
+       :key="fugu1.name"
+       :prepend-icon="fugu1.icon"
+       no-action
+       :append-icon="fugu1.lists ? undefined : ''">
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title>{{ fugu1.name }}</v-list-item-title>
+          </v-list-item-content>
+        </template>
+        <v-list-item v-for="list in fugu1.lists" :key="list">
+          <v-list-item-content>
+            <v-list-item-title> {{ list }}</v-list-item-title>
+          </v-list-item-content>   
+     </v-list-item>
+   </v-list-group>
+   </v-list>
+  </v-container>
+    </v-navigation-drawer>
+
+    <v-app-bar color="primary" dark app>
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>vuetify</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text>串カツ</v-btn>
+
+        <v-menu offset-y>
+          <template v-slot:activator="{on}">
+          <v-btn v-on="on" text>フグ刺し
+            <v-icon>mdi-menu-down</v-icon>
+          </v-btn>
+
+          </template>
+
+          <v-list>
+            <v-subheader>ヘルプ</v-subheader>
+            <v-list-item v-for="fugu in fugus" :key="fugu">
+              <v-list-item-icon>
+               <v-icon>{{ fugu.icon }}</v-icon>
+             </v-list-item-icon>
+
+             <v-list-item-content>
+               <v-list-item-title>{{ fugu.name }}</v-list-item-title>
+             </v-list-item-content>
+
+            </v-list-item>
+          </v-list>
+        </v-menu>
+
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-footer color="primary" dark app>
+      Vuetify
+    </v-footer>
+  </v-app>
+</template>
+<script>
+export default{
+  data(){
+    return{
+       drawer: null,
+      fugus:[
+        {name: '大阪',icon: 'mdi-vuetify'},
+        {name: '神戸',icon: 'mdi-discord' },
+        {name: '山口',icon: 'mdi-bug'},
+        {name: '名古屋',icon: 'mdi-github'},
+        {name: '北海道',icon: 'mdi-stack-overflow'}
+      ],
+      fugus1:[
+        {name: '大阪',icon: 'mdi-vuetify',lists: ['なんでやねん！','アカン！']},
+        {name: '神戸',icon: 'mdi-discord' },
+        {name: '山口',icon: 'mdi-bug',lists: ['萩の月','秋吉台','フグ']},
+        {name: '名古屋',icon: 'mdi-github'},
+        {name: '北海道',icon: 'mdi-stack-overflow',lists: ['蟹','ウニ']}
+      ]
+    }
+  }
+}
+
+</script>
